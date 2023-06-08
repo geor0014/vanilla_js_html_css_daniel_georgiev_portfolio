12d1f77196847115dc89d4c7b6223e445a40e88d
danielgeorgievl098 <geor0014@hz.nl>
2023-06-08 14:23:24 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function openSideMenu() {
+  document.getElementById("menu").style.width = "200px";
+  document.getElementsByClassName("sidebar").style.marginLeft = "200px";
+}
+function closeSideMenu() {
+  document.getElementById("menu").style.width = "0";
+  document.getElementsByClassName("sidebar").style.marginLeft = "0";
+}
